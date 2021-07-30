6aed218a9541e08b6c1eb5ae8e567984dfd23dcc
Yuyuhita Guzman <yuyuhita.guzman@gmail.com>
2021-07-29 22:07:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName='bob'
+
+const leastFavoriteCustomer= 'Joe'
+
+function upperCaseCustomerName(){
+    customerName='BOB'
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= 'Jim'
+}
